1530ed3f8a867f4898c425ad57a743b1ef9a55aa
mlawrence <mlawrence@alsde.edu>
2018-02-09 15:36:18 -0600

    implement file spec view

--- Aden.Web/Controllers/HomeController.cs
+++ Aden.Web/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using ADEN.Web.Core;
+using ADEN.Web.Data;
+using System.Web.Mvc;
 
 namespace Aden.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UnitOfWork uow;
+
+        public HomeController()
+        {
+            var context = AdenContext.Create();
+            uow = new UnitOfWork(context);
+        }
+
         public ActionResult FileSpecifications()
         {
-            return View();
+            var specs = uow.FileSpecifications.GetAllWithReports();
+            return View(specs);
         }
 
     }
---
+++ Aden.Web/Helpers/BoolHtmlHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ADEN.Web.Helpers
+{
+    public static class Helper
+    {
+
+        public static string ToFriendlyString(this Boolean b)
+        {
+            return b ? "Yes" : "No";
+        }
+
+        //public static string BoolLabel(this HtmlHelper, string target, string text)
+        //{
+        //    return string.Format()
+        //}
+
+        public static MvcHtmlString YesNo(this HtmlHelper htmlHelper, bool yesNo)
+        {
+            var text = yesNo ? "Yes" : "No";
+            return new MvcHtmlString(text);
+        }
+
+    }
+}
