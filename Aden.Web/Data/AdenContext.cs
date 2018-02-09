09353baa270fe14c8db49d8965797cd2899346e9
mlawrence <mlawrence@alsde.edu>
2018-02-09 15:07:36 -0600

    implement ef and configurations

---
+++ Aden.Web/Data/AdenContext.cs
@@ -0,0 +1,44 @@
+﻿using ADEN.Web.Data.EntityConfigurations;
+using ADEN.Web.Models;
+using System;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace ADEN.Web.Data
+{
+    public class AdenContext : DbContext
+    {
+        public AdenContext()
+            : base("AdenContext")
+        {
+            Database.Log = msg => Debug.WriteLine(msg);
+            Database.SetInitializer<AdenContext>(null);
+        }
+
+        public static AdenContext Create()
+        {
+            return new AdenContext();
+        }
+
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<FileSpecification> FileSpecifications { get; set; }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<ReportDocument> ReportDocuments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Properties<string>().Configure(c => c.HasColumnType("varchar").HasMaxLength(255));
+            modelBuilder.Properties<string>();
+
+            modelBuilder.Properties<DateTime>().Configure(c => c.HasColumnType("smalldatetime"));
+
+
+            modelBuilder.Configurations.Add(new ReportConfiguration());
+            modelBuilder.Configurations.Add(new FileSpecificationConfiguration());
+            modelBuilder.Configurations.Add(new WorkItemConfiguration());
+            modelBuilder.Configurations.Add(new ReportDocumentConfiguration());
+        }
+    }
+}
---
+++ Aden.Web/Data/EntityConfigurations/FileSpecificationConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using ADEN.Web.Models;
+
+namespace ADEN.Web.Data.EntityConfigurations
+{
+    public class FileSpecificationConfiguration : EntityTypeConfiguration<FileSpecification>
+    {
+        public FileSpecificationConfiguration()
+        {
+            ToTable("Aden.FileSpecifications");
+            Property(s => s.Id).HasColumnName("FileSpecificationId");
+            Property(s => s.FileNumber).HasMaxLength(8);
+            Property(s => s.FileName).HasMaxLength(250);
+            Property(s => s.Version).HasMaxLength(5);
+            //Property(s => s.FileNameFormat).HasMaxLength(50);
+            //Property(s => s.DueDate).HasColumnType("datetime2");
+
+        }
+    }
+}
