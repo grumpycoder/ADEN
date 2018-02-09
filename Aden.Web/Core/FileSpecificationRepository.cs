8c5a5327fd15a8ba59638fb92e26ffc19ff4578a
mlawrence <mlawrence@alsde.edu>
2018-02-09 15:12:58 -0600

    implement core uow and repositories

---
+++ Aden.Web/Core/FileSpecificationRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ADEN.Web.Data;
+using ADEN.Web.Models;
+using Z.EntityFramework.Plus;
+
+namespace ADEN.Web.Core
+{
+    public class FileSpecificationRepository
+    {
+        private readonly AdenContext _context;
+
+        public FileSpecificationRepository(AdenContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<FileSpecification> GetAll()
+        {
+            return _context.FileSpecifications.OrderBy(d => d.DueDate).ToList();
+        }
+
+        public FileSpecification GetById(int id)
+        {
+            return _context.FileSpecifications.SingleOrDefault(x => x.Id == id);
+        }
+
+        //public IEnumerable<FileSpecification> GetAllWithAllReports()
+        //{
+        //    return _context.FileSpecifications.Include(r => r.Reports).OrderBy(d => d.DueDate).ToList();
+        //}
+
+        public IEnumerable<FileSpecification> GetAllWithReports()
+        {
+            var specs = _context.FileSpecifications.Include(r => r.Reports).IncludeFilter(r => r.Reports.Where(x => x.DataYear == r.DataYear)).ToList();
+            return specs;
+        }
+
+        public FileSpecification GetByNumber(string fileNumber)
+        {
+            return _context.FileSpecifications.SingleOrDefault(f => f.FileNumber == fileNumber);
+        }
+    }
+}
---
+++ Aden.Web/Core/UnitOfWork.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using ADEN.Web.Data;
+using ADEN.Web.Helpers;
+using ADEN.Web.Models;
+
+namespace ADEN.Web.Core
+{
+    public class UnitOfWork
+    {
+        private readonly AdenContext _context;
+        public FileSpecificationRepository FileSpecifications { get; set; }
+        public ReportRepository Reports { get; set; }
+        public WorkItemRepository WorkItems { get; set; }
+        public DocumentRepository Documents { get; set; }
+
+        public UnitOfWork(AdenContext context)
+        {
+            _context = context;
+            FileSpecifications = new FileSpecificationRepository(context);
+            Reports = new ReportRepository(context);
+            WorkItems = new WorkItemRepository(context);
+            Documents = new DocumentRepository(context);
+        }
+
+        public OperationResult GenerateDocuments(int reportId)
+        {
+            var report = _context.Reports.Include(r => r.FileSpecification).Include(r => r.Documents).SingleOrDefault(r => r.Id == reportId);
+
+            if (report == null) return new OperationResult("Unable to generate document. Report not found", false);
+
+            if (report.FileSpecification.IsSCH)
+            {
+                ExecuteDocumentCreation(out var dataTable, report.FileSpecification, "SCH");
+                var file = dataTable.ToCSVBytes();
+                report.CreateDocument(file, ReportLevel.SCH);
+            }
+            if (report.FileSpecification.IsLEA)
+            {
+                ExecuteDocumentCreation(out var dataTable, report.FileSpecification, "LEA");
+                var file = dataTable.ToCSVBytes();
+                report.CreateDocument(file, ReportLevel.LEA);
+            }
+            if (report.FileSpecification.IsSEA)
+            {
+                ExecuteDocumentCreation(out var dataTable, report.FileSpecification, "SEA");
+                var file = dataTable.ToCSVBytes();
+                report.CreateDocument(file, ReportLevel.SEA);
+            }
+
+            Complete();
+
+            return new OperationResult("Documents created successfully");
+        }
+
+        private void ExecuteDocumentCreation(out DataTable dataTable, FileSpecification specification, string reportLevel)
+        {
+            dataTable = new DataTable();
+            using (var connection = new SqlConnection(_context.Database.Connection.ConnectionString))
+            {
+                using (var cmd = new SqlCommand(specification.ReportAction, connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@DataYear", specification.DataYear);
+                    cmd.Parameters.AddWithValue("@ReportLevel", reportLevel);
+                    var adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(dataTable);
+                }
+            }
+        }
+
+        public void Complete()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
