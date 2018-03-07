8ee05a7eee4ef6a43fc906be6003a6c5c962e51b
mlawrence <mlawrence@alsde.edu>
2018-03-07 13:14:04 -0600

    gpa callback login

--- Aden.Web/Controllers/AccountController.cs
+++ Aden.Web/Controllers/AccountController.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.IO;
-using System.Text;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using System.Xml;
-using System.Xml.Schema;
 using System.Xml.Serialization;
 using ADEN.Web.Models;
 
@@ -11,18 +12,36 @@ namespace Aden.Web.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly string  AccessKey = "34C3A440-B8A8-4235-88AE-EDD2FA2991BC";
+        private readonly string AccessKey = "34C3A440-B8A8-4235-88AE-EDD2FA2991BC";
 
-        public ActionResult Logon(string token)
+        public ActionResult LoginCallback(string token)
         {
+            //TODO: Create user object from webservice call and create auth ticket and claims
             var svc = new IdemService.Service();
             var data = svc.GetUserDetail(AccessKey, token);
-             
+
             var personXml = data.ChildNodes[0].FirstChild;
 
             var person = ConvertNode<Person>(personXml);
-            
-            return RedirectToAction("FileSpecifications", "Home");
+
+            var authTicket = new FormsAuthenticationTicket(
+                2,
+                person.Emails.FirstOrDefault().Address,
+                DateTime.Now,
+                DateTime.Now.AddMinutes(FormsAuthentication.Timeout.TotalMinutes),
+                false,
+                "some token that will be used to access the web service and that you have fetched"
+            );
+            var authCookie = new HttpCookie(
+                FormsAuthentication.FormsCookieName,
+                FormsAuthentication.Encrypt(authTicket)
+            )
+            {
+                HttpOnly = true
+            };
+            Response.AppendCookie(authCookie);
+            return RedirectToAction("Submissions", "Home");
+            //return null;
         }
 
         private static T ConvertNode<T>(XmlNode node) where T : class
@@ -45,6 +64,6 @@ namespace Aden.Web.Controllers
 
 
         }
-        
+
     }
 }
---
+++ Aden.Web/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(Aden.Web.Startup))]
+namespace Aden.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            var OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthServerProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+
+                // Only do this for demo!!
+                AllowInsecureHttp = true
+            };
+            app.UseOAuthAuthorizationServer(OAuthOptions);
+            app.UseOAuthBearerAuthentication(
+                new OAuthBearerAuthenticationOptions());
+        }
+
+    }
+
+    public class ApplicationOAuthServerProvider
+        : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(
+            OAuthValidateClientAuthenticationContext context)
+        {
+            // This call is required...
+            // but we're not using client authentication, so validate and move on...
+            await Task.FromResult(context.Validated());
+        }
+
+
+        public override async Task GrantResourceOwnerCredentials(
+            OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // DEMO ONLY: Pretend we are doing some sort of REAL checking here:
+            if (context.Password != "password")
+            {
+                context.SetError(
+                    "invalid_grant", "The user name or password is incorrect.");
+                context.Rejected();
+                return;
+            }
+
+            // Create or retrieve a ClaimsIdentity to represent the 
+            // Authenticated user:
+            ClaimsIdentity identity =
+                new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("user_name", context.UserName));
+
+            // Identity info will ultimately be encoded into an Access Token
+            // as a result of this call:
+            context.Validated(identity);
+        }
+    }
+}
