acc2deb390680f0fefed99b82321348be7b54379
mlawrence <mlawrence@alsde.edu>
2018-04-17 13:47:24 -0500

    html helpers for status banner

--- Aden.Web/Helpers/HtmlHelpers.cs
+++ Aden.Web/Helpers/HtmlHelpers.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data.Common;
 using System.Reflection;
+using System.Security.Claims;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,65 +10,103 @@ namespace Aden.Web.Helpers
 {
     public static class HtmlHelpers
     {
+        public static IHtmlString RenderConfigurationValue(this HtmlHelper htmlHelper, string key)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+
+            return new MvcHtmlString(value);
+        }
+
         public static IHtmlString RenderDataSource(this HtmlHelper htmlHelper)
         {
             var connectionString = ConfigurationManager.ConnectionStrings["AdenContext"].ConnectionString;
-            var builder = new DbConnectionStringBuilder();
-            builder.ConnectionString = connectionString;
+            var builder = new DbConnectionStringBuilder { ConnectionString = connectionString };
             return new MvcHtmlString(builder["Data Source"].ToString());
         }
 
         public static IHtmlString RenderDataName(this HtmlHelper htmlHelper)
         {
             var connectionString = ConfigurationManager.ConnectionStrings["AdenContext"].ConnectionString;
-            var builder = new DbConnectionStringBuilder();
-            builder.ConnectionString = connectionString;
+            var builder = new DbConnectionStringBuilder { ConnectionString = connectionString };
             return new MvcHtmlString(builder["initial catalog"].ToString());
         }
 
-
         public static IHtmlString RenderApplicationName(this HtmlHelper htmlHelper)
         {
-            var title = string.Empty;
             var appInstance = htmlHelper.ViewContext.HttpContext.ApplicationInstance;
 
-            var attr = appInstance.GetType().BaseType.Assembly.GetAssemblyAttribute<AssemblyTitleAttribute>(); 
+            var memberInfo = appInstance.GetType().BaseType;
+            if (memberInfo != null)
+            {
+                var attr = memberInfo.Assembly.GetAssemblyAttribute<AssemblyTitleAttribute>();
 
-            //Assembly currentAssembly = GetEntryAssembly();
-            //var attr = currentAssembly.GetAssemblyAttribute<AssemblyTitleAttribute>();
+                return new MvcHtmlString(attr.Title ?? "Unknown");
+            }
+            return new MvcHtmlString("Unknown");
+        }
 
-            //Assembly currentAssembly = Assembly.GetEntryAssembly();
-            //if (currentAssembly == null)
-            //    currentAssembly = Assembly.GetCallingAssembly();
+        public static IHtmlString UserNavBarMenuList(this HtmlHelper htmlHelper, string position)
+        {
+            var identity = ((ClaimsIdentity)HttpContext.Current.User.Identity);
+            var sb = new StringBuilder();
+            var env = ConfigurationManager.AppSettings["ASPNET_ENV"].ToLower();
+            var baseUrl = "https://devaim.alsde.edu/";
+            switch (env)
+            {
+                case "test":
+                    {
+                        baseUrl = baseUrl.Replace("dev", "test");
+                        break;
+                    }
+                case "stage":
+                    {
+                        baseUrl = baseUrl.Replace("dev", "stage");
+                        break;
+                    }
+                case "production":
+                    {
+                        baseUrl = baseUrl.Replace("dev", "");
+                        break;
+                    }
+            }
+            sb.AppendFormat("<ul class='nav navbar-nav navbar-{0}'>", position);
 
-            //object[] attrs = currentAssembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), true);
+            sb.Append(@"<li class='dropdown'>");
+            sb.AppendFormat(
+                @"<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><i class='fa fa-user'></i>&nbsp;{0}<span class='caret'></span></a>",
+                identity.Name);
 
-            //title = attrs[0].ToString();
-            return new MvcHtmlString(attr.Title);
+            sb.Append("<ul class='dropdown-menu'>");
 
-            //Assembly currentAssembly = Assembly.GetEntryAssembly();
-            //var attributes = currentAssembly.CustomAttributes.GetType().GetTypeInfo().Assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute));
-            //var assemblyTitleAttribute = attributes.SingleOrDefault() as AssemblyTitleAttribute;
+            sb.AppendFormat("<li><a href='{0}aim/ApplicationInventory.aspx'><i class='fa fa-home'></i> My Applications</a></li>", baseUrl);
+            sb.AppendFormat("<li><a href='{0}aim/UserProfile.aspx'><i class='fa fa-book'></i> User Profile</a></li>", baseUrl);
+            sb.AppendFormat("<li><a href='{0}aim/EdDirPositions.aspx'><i class='fa fa-university'></i> EdDir Positions</a></li>", baseUrl);
+            sb.Append("<li role='separator' class='divider'></li>");
 
+            sb.Append("<li class='dropdown-header'>AIM Groups and Users</li>");
+            sb.AppendFormat("<li><a href='{0}aim/admin/RolesAndUsers.aspx'><i class='fa fa-group'></i> Groups and Users</a></li>", baseUrl);
+            sb.AppendFormat("<li><a href='{0}aim/admin/UserMaintenance.aspx'><i class='fa fa-user'></i> User Maintenance</a></li>", baseUrl);
+            sb.AppendFormat("<li><a href='{0}aim/alsde/AppMembership.aspx'><i class='fa fa-heartbeat'></i> App Members</a></li>", baseUrl);
+            sb.Append("<li role='separator' class='divider'></li>");
 
-            //var appInstance = htmlHelper.ViewContext.HttpContext.ApplicationInstance;
-            ////given that you should then be able to do 
-            //var assemblyVersion = appInstance.GetType().BaseType.Assembly.GetName().Name;
-            ////note the use of the BaseType - see note below
-            //return new MvcHtmlString(assemblyVersion.ToString());
+            sb.Append("<li class='dropdown-header'>AIM Administration</li>");
+            sb.AppendFormat("<li><a href='{0}aim/admin/EditMessages.aspx'><i class='fa fa-comment'></i> Messages</a></li>", baseUrl);
+            sb.AppendFormat("<li><a href='{0}aim/admin/WebsitesandApplications.aspx'><i class='fa fa-sitemap'></i> Websites and Applications</a></li>", baseUrl);
+            sb.AppendFormat("<li><a href='{0}aim/admin/Groups.aspx''><i class='fa fa-cogs'></i> Group/Subgroup Maintenance</a></li>", baseUrl);
+            sb.AppendFormat("<li><a href='{0}aim/alsde/LoadGroups.aspx'><i class='fa fa-cogs'></i> Load Groups</a></li>", baseUrl);
+            sb.Append("<li role='separator' class='divider'></li>");
 
-            //var title = string.Empty;
-            //Assembly currentAssembly = Assembly.GetEntryAssembly();
-            //if (currentAssembly == null)
-            //    currentAssembly = Assembly.GetCallingAssembly();
+            sb.AppendFormat("<li><a href='{0}aim/Index.aspx?Impersonate=0'><i class='fa fa-stop'></i> Stop Impersonating</a></li>", baseUrl);
+            sb.Append("<li role='separator' class='divider'></li>");
 
-            //var targetValue = currentAssembly.GetCustomAttributes(
-            //    typeof(AssemblyTitleAttribute), inherit: true);
-            //if (targetValue.Length != 0)
-            //    title = ((AssemblyTitleAttribute)targetValue[0]).Title;
+            sb.Append("<li><a href='#'><i class='fa fa-sign-out'></i> Logout</a></li>");
+            sb.Append("</ul>");
 
-            //var name = currentAssembly.GetName().Name;
-            //return new MvcHtmlString(title);
+            sb.Append("<li><a><i class='fa fa-sign-out'></i> Logout</a></li>");
+
+            sb.Append("</ul>");
+
+            return MvcHtmlString.Create(sb.ToString());
         }
     }
 
--- Aden.Web/Properties/AssemblyInfo.cs
+++ Aden.Web/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2018.4.217.6682")]
-[assembly: AssemblyFileVersion("2018.4.217.6682")]
+[assembly: AssemblyVersion("2018.4.229.6682")]
+[assembly: AssemblyFileVersion("2018.4.229.6682")]
 
-[assembly: AssemblyInformationalVersion("2018.4.43")]
+[assembly: AssemblyInformationalVersion("2018.4.55")]
