4a6229d8e8f2e73708cfa04e5a8de9d00fd7af51
mlawrence <mlawrence@alsde.edu>
2018-05-04 11:06:04 -0500

    update to 4.7.1 framework

--- Aden.Core/Properties/AssemblyInfo.cs
+++ Aden.Core/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.5.122.6698")]
-[assembly: AssemblyFileVersion("2018.5.122.6698")]
+[assembly: AssemblyVersion("2018.5.123.6699")]
+[assembly: AssemblyFileVersion("2018.5.123.6699")]
