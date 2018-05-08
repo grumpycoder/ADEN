402a21ae5d765dd3a77cacd183c898bcdb66b333
mlawrence <mlawrence@alsde.edu>
2018-05-08 08:38:23 -0500

    refactor work item create method

--- Aden.Core/Properties/AssemblyInfo.cs
+++ Aden.Core/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.5.168.6702")]
-[assembly: AssemblyFileVersion("2018.5.168.6702")]
+[assembly: AssemblyVersion("2018.5.172.6703")]
+[assembly: AssemblyFileVersion("2018.5.172.6703")]
--- Aden.Web/Properties/AssemblyInfo.cs
+++ Aden.Web/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2018.5.439.6702")]
-[assembly: AssemblyFileVersion("2018.5.439.6702")]
+[assembly: AssemblyVersion("2018.5.443.6703")]
+[assembly: AssemblyFileVersion("2018.5.443.6703")]
 
-[assembly: AssemblyInformationalVersion("2018.5.265")]
+[assembly: AssemblyInformationalVersion("2018.5.269")]
