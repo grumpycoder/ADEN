3744afd511d8e251ea268ad0c009607286517cdd
mlawrence <mlawrence@alsde.edu>
2018-06-11 13:34:13 -0500

    Implement report dto

---
+++ Aden.Core/Dtos/ReportDocumentDto.cs
@@ -0,0 +1,12 @@
+﻿using Aden.Core.Models;
+using Heroic.AutoMapper;
+
+namespace Aden.Core.Dtos
+{
+    public class ReportDocumentDto : IMapFrom<ReportDocument>
+    {
+        public int Id { get; set; }
+        //public byte[] File { get; set; }
+        public string Filename { get; set; }
+    }
+}
--- Aden.Web/Controllers/ReportsController.cs
+++ Aden.Web/Controllers/ReportsController.cs
@@ -1,4 +1,5 @@
-﻿using Aden.Core.Models;
+﻿using Aden.Core.Dtos;
+using Aden.Core.Models;
 using Aden.Core.Repositories;
 using Aden.Core.Services;
 using Aden.Web.ViewModels;
@@ -33,7 +34,7 @@ namespace Aden.Web.Controllers
         public async Task<object> Get(int datayear, string filenumber)
         {
             var reports = await _uow.Reports.GetByFileSpecificationAsync(datayear, filenumber);
-            var reportList = Mapper.Map<List<ReportViewModel>>(reports);
+            var reportList = Mapper.Map<List<ReportDto>>(reports);
             return Ok(reportList);
         }
 
