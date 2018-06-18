9f35f86692cfc8e37a13c6ccf53863b749ba8c92
mlawrence <mlawrence@alsde.edu>
2018-06-18 13:33:21 -0500

    implement html table view of csv data

--- Aden.Core/Dtos/ReportDocumentDto.cs
+++ Aden.Core/Dtos/ReportDocumentDto.cs
@@ -1,6 +1,9 @@
-﻿using Aden.Core.Models;
+﻿using System.Collections.Generic;
+using Aden.Core.Helpers;
+using Aden.Core.Models;
 using AutoMapper;
 using Heroic.AutoMapper;
+using Nortal.Utilities.Csv;
 using System.Text;
 
 namespace Aden.Core.Dtos
@@ -18,11 +21,45 @@ namespace Aden.Core.Dtos
         private string ConvertToHtml()
         {
             var sb = new StringBuilder();
-            sb.Append("<table><thead></thead>");
 
-            sb.Append("</table>");
-            return Content;
+            sb.Append("<table class='table table-condensed table-responsive table-striped table-bordered'><thead></thead><tbody>");
+
+            using (var parser = new CsvParser(Content))
+            {
+                var headerRow = parser.ReadNextRow();
+                var firstRow = parser.ReadNextRow();
+
+                sb.Append(CreateTableRow(headerRow, firstRow.Length));
+                sb.Append(CreateTableRow(firstRow));
+
+                var rows = parser.ReadToEnd();
+                foreach (var row in rows)
+                {
+                    sb.Append(CreateTableRow(row));
+                }
+            }
+
+
+            sb.Append("</tbody></table>");
+            return sb.ToString();
         }
+
+        private string CreateTableRow(IReadOnlyCollection<string> row, int? numberCols = null)
+        {
+            var extraCols = numberCols - row.Count;
+
+            var sb = new StringBuilder();
+            sb.Append("<tr>");
+            foreach (var col in row)
+            {
+                sb.AppendFormat($"<td>{col}</td>");
+            }
+
+            sb.Append("<td></td>".Repeat(extraCols ?? 0));
+            sb.Append("</tr>");
+            return sb.ToString();
+        }
+
 
         public void CreateMappings(IMapperConfigurationExpression configuration)
         {
