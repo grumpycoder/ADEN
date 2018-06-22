0566e7b8a7f89a7fabbfcb9c161ea6b8b49dc289
mlawrence <mlawrence@alsde.edu>
2018-06-22 16:00:49 -0500

    modal notification of expired login on all ajax requests

--- Aden.Web/Properties/AssemblyInfo.cs
+++ Aden.Web/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2018.0622.2.6719")]
-[assembly: AssemblyFileVersion("2018.0622.2.6719")]
+[assembly: AssemblyVersion("2018.0622.4.6719")]
+[assembly: AssemblyFileVersion("2018.0622.4.6719")]
 
-[assembly: AssemblyInformationalVersion("2018.622.2")]
+[assembly: AssemblyInformationalVersion("2018.622.4")]
