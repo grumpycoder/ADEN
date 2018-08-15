7de81228655618338ab2db7fe5de944c261b6f2a
mlawrence <mlawrence@alsde.edu>
2018-08-15 09:47:14 -0500

    update bootstrap css status bar menu for mobile

--- Aden.Core/Properties/AssemblyInfo.cs
+++ Aden.Core/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.8.13.6800")]
-[assembly: AssemblyFileVersion("2018.8.13.6800")]
+[assembly: AssemblyVersion("2018.8.14.6801")]
+[assembly: AssemblyFileVersion("2018.8.14.6801")]
 
-[assembly: AssemblyInformationalVersion("2018.8.14")]
+[assembly: AssemblyInformationalVersion("2018.8.15")]
