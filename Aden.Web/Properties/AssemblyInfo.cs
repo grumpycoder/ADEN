2b40b467180877f1056f36f94b5c637a0d45eb00
mlawrence <mlawrence@alsde.edu>
2018-08-27 09:31:56 -0500

    update logging library

--- Aden.Web/Properties/AssemblyInfo.cs
+++ Aden.Web/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2018.8.25.6811")]
-[assembly: AssemblyFileVersion("2018.8.25.6811")]
+[assembly: AssemblyVersion("2018.8.27.6814")]
+[assembly: AssemblyFileVersion("2018.8.27.6814")]
 
-[assembly: AssemblyInformationalVersion("2018.8.25")]
+[assembly: AssemblyInformationalVersion("2018.8.27")]
