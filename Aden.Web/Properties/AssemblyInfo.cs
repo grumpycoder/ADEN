3d83e5435ec964035f9c0b23591ee3a2c240e0de
mlawrence <mlawrence@alsde.edu>
2018-09-05 11:33:33 -0500

    implemented glimpse plugin

--- Aden.Web/Properties/AssemblyInfo.cs
+++ Aden.Web/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2018.9.4.6822")]
-[assembly: AssemblyFileVersion("2018.9.4.6822")]
+[assembly: AssemblyVersion("2018.9.5.6823")]
+[assembly: AssemblyFileVersion("2018.9.5.6823")]
 
 [assembly: AssemblyInformationalVersion("2018.8.51")]
