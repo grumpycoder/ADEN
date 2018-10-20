3ffb83d37d06633b40da38155a5152f7aa9b4028
mlawrence <mlawrence@alsde.edu>
2018-10-19 19:31:16 -0500

    missing error view button on work history

--- Aden.Core/Properties/AssemblyInfo.cs
+++ Aden.Core/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("2018.10.20.6867")]
 [assembly: AssemblyFileVersion("2018.10.20.6867")]
 
-[assembly: AssemblyInformationalVersion("2018.10.0-beta-120")]
+[assembly: AssemblyInformationalVersion("2018.10.0-beta-123")]
